4a45f6aae669f76ad6f78b1c8f5d01e79026b704
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-31 15:33:48 +0200

    changed the componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 function App() {
-
   return (
     <>
-     
+      <div>
+        <Header />
+        <Meme />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
